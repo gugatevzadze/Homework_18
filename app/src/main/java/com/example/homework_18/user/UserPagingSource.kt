cd60cecef3d85f1db97887be7a930e10f8ca6c04
gugatevzadze@gmail.com <gugatevzadze@gmail.com>
2023-12-22 08:18:02 +0400

    implemented repository pattern

--- app/src/main/java/com/example/homework_18/user/UserPagingSource.kt
+++ app/src/main/java/com/example/homework_18/user/UserPagingSource.kt
@@ -3,52 +3,33 @@ package com.example.homework_18.user
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.example.homework_18.service.ApiService
+import retrofit2.HttpException
+import java.io.IOException
 
-//class UserPagingSource(private val apiService: ApiService) : PagingSource<Int, User>() {
-//
-//    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, User> {
-//        try {
-//            //extracting page number from the LoadParams or default to page 1
-//            val page = params.key ?: 1
-//            //making an API request to fetch user data for the specified page
-//            val response = apiService.getUsers(page)
-//
-//            //returning a LoadResult containing the loaded data, previous key, and next key
-//            return LoadResult.Page(
-//                data = response.data,
-//                prevKey = if (page == 1) null else page - 1,
-//                nextKey = if (page < response.total_pages) page + 1 else null
-//            )
-//        } catch (e: IOException) {
-//            //handling IOException for network failures
-//            return LoadResult.Error(e)
-//        } catch (e: HttpException) {
-//            //handling HttpException
-//            return LoadResult.Error(e)
-//        } catch (e: Exception) {
-//            //handling other exceptions by returning an error result with a throwable
-//            return LoadResult.Error(Throwable("Error loading page", e))
-//        }
-//    }
-//
-//    override fun getRefreshKey(state: PagingState<Int, User>): Int? {
-//        return state.anchorPosition
-//    }
-//}
 class UserPagingSource(private val apiService: ApiService) : PagingSource<Int, User>() {
+
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, User> {
         try {
-            val nextPageNumber = params.key ?: 1
-            val response = apiService.getUsers(nextPageNumber)
-            val users = response.data
+            //extracting page number from the LoadParams or default to page 1
+            val page = params.key ?: 1
+            //making an API request to fetch user data for the specified page
+            val response = apiService.getUsers(page)
 
+            //returning a LoadResult containing the loaded data, previous key, and next key
             return LoadResult.Page(
-                data = users,
-                prevKey = if (nextPageNumber == 1) null else nextPageNumber - 1,
-                nextKey = if (users.isEmpty()) null else nextPageNumber + 1
+                data = response.data,
+                prevKey = if (page == 1) null else page - 1,
+                nextKey = if (page < response.total_pages) page + 1 else null
             )
-        } catch (e: Exception) {
+        } catch (e: IOException) {
+            //handling IOException for network failures
             return LoadResult.Error(e)
+        } catch (e: HttpException) {
+            //handling HttpException
+            return LoadResult.Error(e)
+        } catch (e: Exception) {
+            //handling other exceptions by returning an error result with a throwable
+            return LoadResult.Error(Throwable("Error loading page", e))
         }
     }
 
